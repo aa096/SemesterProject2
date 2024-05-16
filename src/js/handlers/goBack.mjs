62bd344470527cdd4caeaf171ccdee09f58176a3
aa096 <126397865+aa096@users.noreply.github.com>
2024-05-16 20:45:31 +0200

    Go back mark

---
+++ src/js/handlers/goBack.mjs
@@ -0,0 +1,12 @@
+export function setupGoBack() {
+    const goBackButton = document.querySelector("#goBack");
+  
+    if (goBackButton) {
+      goBackButton.addEventListener("click", goBack);
+    }
+  
+    function goBack() {
+      window.history.back();
+    }
+  }
+  
