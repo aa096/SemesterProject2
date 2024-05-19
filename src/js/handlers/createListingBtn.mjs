eea77616e796ae8fc31876f08d19884fa73970b2
aa096 <126397865+aa096@users.noreply.github.com>
2024-05-19 21:48:49 +0200

    fixed link

--- src/js/handlers/createListingBtn.mjs
+++ src/js/handlers/createListingBtn.mjs
@@ -2,6 +2,6 @@ export function handleCreateListingButtonClick() {
     const createListingButton = document.getElementById("createListingButton");
   
     createListingButton.addEventListener("click", () => {
-      window.location.href = "/auctions/";
+      window.location.href = "/auction/create";
     });
 }
